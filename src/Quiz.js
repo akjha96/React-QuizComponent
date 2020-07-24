288c15bbd942bf744cde416572248e5519188213
akjha96 <akjha96@gmail.com>
2020-07-24 17:16:50 +0530

    Added conditioanl and event Handlers

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,6 +5,9 @@ import QuizEnd from "./QuizEnd";
 let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
+  showNextQuestion = () => {
+    this.setState({ quiz_position: this.state.quiz_position + 1 });
+  };
   constructor(props) {
     super(props);
     this.state = {
@@ -15,7 +18,7 @@ class Quiz extends Component {
   render() {
     const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
-    return <div>{isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}</div>;
+    return <div>{isQuizEnd ? <QuizEnd /> : <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}</div>;
   }
 }
 
